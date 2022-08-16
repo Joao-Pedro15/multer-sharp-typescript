764ef6084da2736c3096fe550fcab889d0142007
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-15 22:46:25 -0300

    feat: create a object from multer config

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -4,5 +4,34 @@ import { randomBytes } from 'crypto'
 import { Request } from 'express'
 
 export const MulterConfig = {
-    dest: resolve(__dirname, '..', '..', 'tmp', 'uploads')
+    dest: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    storage: multer.diskStorage({
+        destination: (request, file:Express.Multer.File, callback)  =>  {
+            callback(null,  resolve(__dirname, '..', '..', 'tmp', 'uploads'))
+        },
+        filename: (request, file, callback) => {
+            randomBytes(16, (err:Error | null, hash:Buffer) => {
+                if(err) return err
+                const fileName = `${hash.toString('hex')}-${file.originalname}`
+                callback(null, fileName)
+            })
+        },
+    }),
+    limits: {
+        fileSize: 2 * 1024 * 1024
+    },
+    fileFilter: (request: Request, file:Express.Multer.File, callback:(Error:Error | null | any , isSuccess: boolean) => void) => {
+        const allowedMimes = [
+            'image/jpeg',
+            'image/pjpeg',
+            'image/png',
+            'image/gif',
+        ]
+        if(allowedMimes.includes(file.mimetype)){
+            callback(null, true)
+        }else {
+            callback(new Error('Invalid file type.'), false)
+        }
+    },
 }
+
