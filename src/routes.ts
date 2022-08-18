cabac1f6b6e3b8ee366b4487034796d7cf195b6a
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-18 20:42:24 -0300

    refactor: route receiving middlewares multer and sharp

--- src/routes.ts
+++ src/routes.ts
@@ -4,18 +4,12 @@ import multer from 'multer'
 import { join } from 'path'
 import sharp from 'sharp'
 import { MulterConfig } from './config/multer'
+import { resizeImage } from './config/sharp'
 
 router.get('/', (request:Request, response: Response) => {
     return response.status(200).json({hello: 'world'})
 })
 
-router.post('/posts', multer(MulterConfig).single('file'), (request: Request, response: Response) => { 
-    let compressImagePath = join(__dirname, '..', 'images', new Date().getTime() + '.jpg')
-    sharp(request.file?.path).resize(640, 480).toFile( compressImagePath, (error) => {
-        if(error)return response.status(400).send(error)
-        return response.status(201).send('Success!')
-    } )
-    return response.status(201)
-})
+router.post('/posts', multer(MulterConfig).single('file'), resizeImage)
 
 export default router 
