8bf66cf9672fc1b04c6b47f12cfbae71f83f8489
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-17 21:32:03 -0300

    feat: create a middleware resize image, but not using

--- src/config/sharp.ts
+++ src/config/sharp.ts
@@ -1,1 +1,15 @@
+import { Response, Request, NextFunction } from 'express';
 import sharp from 'sharp'
+import { resolve } from 'path'
+import { readdirSync } from 'fs'
+
+export async function resizeImage(path:any, width:number) {
+   
+    const pathFile = resolve(__dirname, '..', '..', 'tmp', 'resizeImages')
+    sharp(path).resize(200, 200).toFile(pathFile, (error:Error | null , data:any) => {
+        if(error) {
+            console.log(error)
+            
+        }
+    })   
+}
